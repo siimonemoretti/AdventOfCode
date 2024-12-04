46556d83d8f69587c6392eefdb15e6208c5aef25
Simone <siimonemoretti@gmail.com>
2024-12-04 17:37:09 +0100

    Day4 done.

--- 2024/Day4/main.c
+++ 2024/Day4/main.c
@@ -2,10 +2,19 @@
  * We need to detect all the "XMAS" words present in input.txt
  * They can be in all possible positions: horizontal, vertical and diagonal.
  * Also, they can be reversed.
+ * Part 2:
+ * Now we'll look for both of these patterns:
+ * M . S | S . M
+ * . A . | . A .
+ * S . M | M . S
+ * Where the dots can be any character.
+ * This is an "X" MAS pattern.
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define PART_2
 
 /**
  * @brief This function takes the fp to the input file and reads the file line by line, saving it in a matrix
@@ -31,6 +40,7 @@ char **read_file(FILE *fp, int *rows, int *cols)
    return matrix;
 }
 
+#ifndef PART_2
 int search_xmas(char **matrix, int rows, int cols)
 {
    int cnt = 0;
@@ -69,6 +79,33 @@ int search_xmas(char **matrix, int rows, int cols)
    }
    return cnt;
 }
+#else
+int check_diagonal(char c1, char c2)
+{
+   return (c1 == 'M' && c2 ==  'S') || (c1 == 'S' && c2 == 'M');
+}
+
+int search_xmas(char **matrix, int rows, int cols)
+{
+   int cnt = 0;
+   // This time, i and j start from 1 and terminate at rows - 1 and cols - 1 (the latter isn't really useful)
+   // This way we don't need "if" statements to check if we are at the borders
+   for (int i = 1; i < rows - 1; i++)
+   {
+      for (int j = 1; j < cols; j++)
+      {
+         if (matrix[i][j] == '\0')
+            break;
+         if (matrix[i][j] == 'A')
+         {
+            if(check_diagonal(matrix[i-1][j-1], matrix[i+1][j+1]) && check_diagonal(matrix[i-1][j+1], matrix[i+1][j-1]))
+               cnt++;
+         }
+      }
+   }
+   return cnt;
+}
+#endif
 
 int main(int argc, char **argv)
 {
